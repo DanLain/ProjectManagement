dbf53d15f22c9c1b7f304fb5c4ad4ef46b2e1e4e
gjohns <gary.johns@mavs.uta.edu>
2013-09-19 13:43:35 -0500

    login and out

---
+++ project code/login.php
@@ -0,0 +1,53 @@
+<?php
+session_start();
+
+$_SESSION['Locked'] = "False";
+
+if(!$mysqlconnect){
+	$mysqlconnect=mysqli_connect('localhost','test');
+}
+mysql_select_db("test");
+
+$check_password="Select * from login where (`UserName` LIKE '%".$_REQUEST['Email']."%')";
+$check_result=mysql_fetch_array(mysql_query($check_password));
+if ($check_result['Locked']==0){
+		if ($_REQUEST['Password']<>$check_result['Password']){
+			if($_SESSION['BadLogin']!= $_REQUEST['Email']){
+				$_SESSION['LoginTry'] =0;
+			}
+			$_SESSION['BadLogin'] = $_REQUEST['Email'];
+			$_SESSION['LoginTry'] = $_SESSION['LoginTry'] +1;
+			if ($_SESSION['LoginTry']==5){
+				$update_query="UPDATE login  
+					SET   login.Locked=1
+					WHERE login.UserName = '$_REQUEST[Email]'";
+					mysql_query($update_query);
+			}
+			header("Location: http://localhost/flohome.php");		
+		}	
+		else{
+			$_SESSION['EmployeeLogin'] = ( $check_result['Emp']==0 ? "False":"True");
+			$_SESSION['Login'] = "True";
+			$_SESSION['User'] = $check_result['UserName']; 
+			$_SESSION['CustomerID']  = $check_result['CustomerID'];
+
+			if(isset($_SESSION['ReadyToCheckout'])){
+				if($_SESSION['ReadyToCheckout'] =="True")header("Location: http://checkout.php");
+				else header("Location:http://localhost/flohome.php");
+				}
+			else {
+				header("Location:http://localhost/flohome.php");		
+				}
+		}
+	}
+else {	
+		$_SESSION['Locked'] = "True";
+		$_SESSION['BadLogin'] = $_REQUEST['Email'];
+		$_SESSION['Email'] = $_REQUEST['Email'];
+		header("http://localhost/flohome.php");
+	}
+?>
+
+
+
+
