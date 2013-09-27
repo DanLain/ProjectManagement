f17812c47332b62a1fa547cd9ed7cb9bb6e09da6
gjohns <gary.johns@mavs.uta.edu>
2013-09-27 15:00:07 -0500

    a

--- CodeBase/userInsert.php
+++ CodeBase/userInsert.php
@@ -23,17 +23,17 @@ $new_number="Select * from employee";
 $number_result=mysql_query($new_number);
 $next_employee_ID = mysql_num_rows($number_result) +1;
 
-if ($next_employee_ID > 1){
-		$check_email="Select * from employee";
-		$check_result=mysql_query($check_email);
-		while($row=mysql_fetch_array($check_result)){
-			if($row['Email'] == $_REQUEST['Email']){
-				if ($row['employeeID'] != $_SESSION['employeeID']){
-					$_SESSION['BadEmail'] = "True";
-				}
-			}
-		}
-	}
+// if ($next_employee_ID > 1){
+		// $check_email="Select * from employee";
+		// $check_result=mysql_query($check_email);
+		// while($row=mysql_fetch_array($check_result)){
+			// if($row['Email'] == $_REQUEST['Email']){
+				// if ($row['employeeID'] != $_SESSION['employeeID']){
+					// $_SESSION['BadEmail'] = "True";
+				// }
+			// }
+		// }
+	// }
 $passwordReg="/^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*$/";
 $emailReg="(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|'(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*')@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])";//http://www.regular-expressions.info/email.html
 if (!($_SESSION['Login']=="True" && !strlen($_REQUEST['Password'])>0) && preg_match($passwordReg, $_REQUEST['Password']) !=1){
@@ -48,8 +48,8 @@ if (!($_SESSION['Login']=="True" && !strlen($_REQUEST['Password'])>0) && preg_ma
 		$_SESSION['City'] = $_REQUEST['City'];
 		$_SESSION['State'] = $_REQUEST['State'];
 		$_SESSION['Zip'] = $_REQUEST['Zip'];
-		$_SESSION['CompanyID'] = $_REQUEST['CompanyID'];
 		$_SESSION['Email'] = $_REQUEST['Email'];
+		$_SESSION['CompanyID'] = $_REQUEST['CompanyID'];
 		$_SESSION['PasswordInvalid']="True";
 		$_SESSION['GoodReg']="False";
 		header("Location: http://localhost/registration.php");
@@ -65,8 +65,8 @@ if(!filter_var($_REQUEST['Email'], FILTER_VALIDATE_EMAIL)){
 		$_SESSION['City'] = $_REQUEST['City'];
 		$_SESSION['State'] = $_REQUEST['State'];
 		$_SESSION['Zip'] = $_REQUEST['Zip'];
-		$_SESSION['CompanyID'] = $_REQUEST['CompanyID'];
 		$_SESSION['Email'] = $_REQUEST['Email'];
+		$_SESSION['CompanyID'] = $_REQUEST['CompanyID'];
 		$_SESSION['GoodReg']="False";
 		$_SESSION['BadEmail'] = "True";
 		$_SESSION['EmailInvalid'] = "True";
@@ -83,8 +83,8 @@ if($_SESSION['BadEmail'] == "True"){
 		$_SESSION['City'] = $_REQUEST['City'];
 		$_SESSION['State'] = $_REQUEST['State'];
 		$_SESSION['Zip'] = $_REQUEST['Zip'];
-		$_SESSION['CompanyID'] = $_REQUEST['CompanyID'];
 		$_SESSION['Email'] = $_REQUEST['Email'];
+		$_SESSION['CompanyID'] = $_REQUEST['CompanyID'];
 		$_SESSION['GoodReg']="False";
 		header("Location: http://localhost/registration.php");
 	}
@@ -100,8 +100,8 @@ if($_REQUEST['Password'] != $_REQUEST['ConfirmPassword']){
 		$_SESSION['City'] = $_REQUEST['City'];
 		$_SESSION['State'] = $_REQUEST['State'];
 		$_SESSION['Zip'] = $_REQUEST['Zip'];
-		$_SESSION['CompanyID'] = $_REQUEST['CompanyID'];
 		$_SESSION['Email'] = $_REQUEST['Email'];
+		$_SESSION['CompanyID'] = $_REQUEST['CompanyID'];
 		$_SESSION['GoodReg']="False";
 		header("Location: http://localhost/registration.php");
 	}
@@ -116,8 +116,8 @@ if ($_SESSION['GoodReg']=="True"){
 				   '$_REQUEST[City]',
 				   '$_REQUEST[State]',
 				   '$_REQUEST[Zip]',
-				   '$_REQUEST[CompanyID]',
-				   '$_REQUEST[Email]')";
+				   '$_REQUEST[Email]',
+				   '$_REQUEST[CompanyID]')";
 		mysql_query($insert_query);
 		$insert_query="Insert into login 
 			values('$next_employee_ID',
