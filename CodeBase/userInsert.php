d339d06b850c2c2a4ff10505d860294ef6903df9
DanLain <daniel.lain@mavs.uta.edu>
2013-10-07 16:32:09 -0500

    fixed user insert

--- CodeBase/userInsert.php
+++ CodeBase/userInsert.php
@@ -19,21 +19,16 @@ else {
 	}
 
 
-$new_number="Select * from employee";
-$number_result=mysql_query($new_number);
-$next_employee_ID = mysql_num_rows($number_result) +1;
+	$check_email="Select * from employee";
+	$check_result=mysql_query($check_email);
+	while($row=mysql_fetch_array($check_result)){
+		if($row['Email'] == $_REQUEST['Email']){
+			if ($row['EmployeeID'] != $_SESSION['EmployeeID']){
+				$_SESSION['BadEmail'] = "True";
+			}
+		}
+	}
 
- if ($next_employee_ID > 1){
-		 $check_email="Select * from employee";
-		$check_result=mysql_query($check_email);
-		 while($row=mysql_fetch_array($check_result)){
-			 if($row['Email'] == $_REQUEST['Email']){
-				 if ($row['employeeID'] != $_SESSION['employeeID']){
-					 $_SESSION['BadEmail'] = "True";
-				 }
-			 }
-		 }
-	 }
 $passwordReg="/^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*$/";
 $emailReg="(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|'(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*')@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])";//http://www.regular-expressions.info/email.html
 if (!($_SESSION['Login']=="True" && !strlen($_REQUEST['Password'])>0) && preg_match($passwordReg, $_REQUEST['Password']) !=1){
