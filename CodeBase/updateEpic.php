df151142a730971c1363d9334f6af30dc63dee22
DanLain <daniel.lain@mavs.uta.edu>
2013-10-08 13:41:34 -0500

    Updated Properties

---
+++ CodeBase/updateEpic.php
@@ -0,0 +1,20 @@
+<html>
+<body>
+<?php
+
+session_start();
+if(!isset($mysqlconnect)){
+			$mysqlconnect=mysql_connect('localhost','danla_web','thisiscool');
+			
+		}
+		mysql_select_db("danlain_live");
+		$insertQuery="Insert company
+			SET ProjectID='$_REQUEST[ProjectID]', Fax='$_REQUEST[ProjectID]',
+			Name='$_REQUEST[Name]', 
+			Description='$_REQUEST[Description]'";
+		mysql_query($insertQuery);
+		//header("Location: http://localhost/companyProperties.php");
+?>
+
+</body>
+</html>
